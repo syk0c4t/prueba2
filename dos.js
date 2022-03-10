38c772a9310e3fa47a31f068fed518b57b70093e
syk0c4t <100176680+syk0c4t@users.noreply.github.com>
2022-03-09 20:53:11 -0600

    prueba 2

---
+++ dos.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() { background(255); 
+      fill(0,0,255); 
+      rect(30,300,200,200); 
+      fill(0,150,75); 
+      rect(20,100,50,50); 
+      fill(255,90,0); 
+      rect(40,200,100,50); 
+      fill(255,0,0);
+      rect(300,350,50,200); }
